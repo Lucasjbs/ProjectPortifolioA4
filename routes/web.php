f0bfd8422b0de46367c36d00aec8fb84d5940e85
Lucas J Bastos <lucasjbastos26@gmail.com>
2024-06-04 17:00:58 -0300

    new: homepage-basics

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
+use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
@@ -18,10 +19,57 @@ Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::get('/testboard', function () {
+    //https://laravel.com/docs/11.x/installation#next-steps
+    $environment = "Unknown";
+    if (App::environment(['staging'])) {
+        $environment = "Staging";
+        // php artisan about --only=environment
+        // https://laravel.com/docs/11.x/configuration#determining-the-current-environment
+    }
+
+    return Inertia::render('Testboard', ['environment' => $environment, 'new' => "New Text"]);
+})->name('testboard');
+
+Route::get('/homepage', function () {
+    return Inertia::render('Homepage', [
+        'canLogin' => Route::has('login'),
+        'canRegister' => Route::has('register'),
+    ]);
+})->name('homepage');
+
+Route::get('/portfolio', function () {
+    return Inertia::render('Homepage', [
+        'canLogin' => Route::has('login'),
+        'canRegister' => Route::has('register'),
+    ]);
+})->name('portfolio');
+
+Route::get('/certifications', function () {
+    return Inertia::render('Homepage', [
+        'canLogin' => Route::has('login'),
+        'canRegister' => Route::has('register'),
+    ]);
+})->name('certifications');
+
+Route::get('/articles', function () {
+    return Inertia::render('Homepage', [
+        'canLogin' => Route::has('login'),
+        'canRegister' => Route::has('register'),
+    ]);
+})->name('articles');
+
+Route::get('/projects', function () {
+    return Inertia::render('Homepage', [
+        'canLogin' => Route::has('login'),
+        'canRegister' => Route::has('register'),
+    ]);
+})->name('projects');
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
